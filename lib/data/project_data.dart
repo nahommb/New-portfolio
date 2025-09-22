cc2bd4937874dc84bebfd6c0e2ab1c7c76141c5f
nahom <nahomjr17@gmail.com>
2025-09-22 04:29:58 -0500

    updated

--- lib/data/project_data.dart
+++ lib/data/project_data.dart
@@ -5,21 +5,21 @@ class ProjectData {
     'description':'In the Niya Sports Wear project, I faced a complex issue with state ',
     'technology':['Flutter', 'Firebase', 'Node.js', 'Express.js', 'MongoDB'],
     'previewLink':'https://niyasportswear.netlify.app/',
-    'imagePath':'images/project/niya.PNG'
+    'imagePath':'assets/images/project/niya.PNG'
   },
   {
     'title':'Project 2',
     'description':'Description for Project 2',
     'technology':['Tech1, Tech2, Tech3'],
     'previewLink':'https://example.com/project2',
-    'imagePath':'imagesproject/project2.png'
+    'imagePath':'assets/imagesproject/project2.png'
   },
   {
     'title':'Project 3',
     'description':'Description for Project 3',
     'technology':['TechA, TechB, TechC'],
     'previewLink':'https://example.com/project3',
-    'imagePath':'images/project/project3.png'
+    'imagePath':'assets/images/project/project3.png'
   }
 ];
 
@@ -29,7 +29,7 @@ static const List <dynamic> mobileProjectDataList = [
     'description':'In the Niya Sports Wear project, I faced a complex issue with state ',
     'technology':['Flutter', 'Firebase', 'Node.js', 'Express.js', 'MongoDB'],
     'previewLink':'https://niyasportswear.netlify.app/',
-    'imagePath':'images/project/niya.PNG'
+    'imagePath':'assets/images/project/niya.PNG'
 }
 ];
 
@@ -37,7 +37,7 @@ static const List <dynamic> uiuxProjectDataList = [{
     'title':'Niya Sports wear',
     'description':'In the Niya Sports Wear project, I faced a complex issue with state ',
     'imagePath':['assets/images/project/personal-portfolio.png','assets/images/project/personal-portfolio-2.png',
-                  'assetsimages/project/personal-portfolio.png','assets/images/project/personal-portfolio.png'
+                  'assets/images/project/personal-portfolio.png','assets/images/project/personal-portfolio.png'
                   ]
 
 },
