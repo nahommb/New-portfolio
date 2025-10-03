3d4514db2e7c5157fba761ee7f5fe187feb84b45
nahom <nahomjr17@gmail.com>
2025-10-03 03:41:43 -0500

    up

--- lib/data/project_data.dart
+++ lib/data/project_data.dart
@@ -5,28 +5,28 @@ class ProjectData {
     'description':'is a modern e-commerce website designed to showcase and sell high-quality athletic and casual wear. The site provides a clean, user-friendly shopping experience with a focus on style, comfort, and performance. Visitors can easily browse collections, view product details, and explore trendy sportswear designed for both active lifestyles and everyday fashion.',
     'technology':['React', 'Redux', 'Node.js', 'Express.js', 'MongoDB'],
     'previewLink':'https://niyasportswear.netlify.app/',
-    'imagePath':'/images/project/niyasports.png'
+    'imagePath':'assets/images/project/niyasports.png'
   },
   {
     'title':'Record Communication website',
     'description':'esigned to showcase marketing services, campaigns, and brand strategies in a professional and engaging way. The platform highlights key offerings such as SEO, social media management, content creation, and analytics through a modern, responsive design.',
     'technology':['React',' Node.js', 'MySQL', 'Express.js'],
     'previewLink':'https://recordcommunication.netlify.app/',
-    'imagePath':'/images/project/recordcommunication.png'
+    'imagePath':'assets/images/project/recordcommunication.png'
   },
   {
     'title':'Pofeex website',
     'description':'Pofeex is a cutting-edge freelancing platform that connects businesses and individuals with skilled professionals across various fields. The website offers a seamless experience for posting projects, browsing freelancer profiles, and managing contracts, all within a secure and user-friendly environment.',
     'technology':['Flutter,Dart'],
     'previewLink':'https://pofeex.netlify.app/',
-    'imagePath':'/images/project/pofeex.png'
+    'imagePath':'assets/images/project/pofeex.png'
   },
     {
     'title':'Yegna home website clone',
     'description':'Yegna Home is a modern real estate platform that connects buyers, sellers, and renters with properties that meet their needs. The website features advanced search capabilities, detailed property listings, and user-friendly navigation to facilitate seamless transactions in the real estate market.',
     'technology':['React'],
     'previewLink':'https://yegnahomeclone.netlify.app/',
-    'imagePath':'/images/project/yegnahome.png'
+    'imagePath':'assets/images/project/yegnahome.png'
   }
 ];
 
@@ -58,15 +58,15 @@ static const List <dynamic> mobileProjectDataList = [
 static const List <dynamic> uiuxProjectDataList = [{
     'title':'Edutainment App UIUX Design',
     'description':'In the Niya Sports Wear project, I faced a complex issue with state ',
-    'imagePath':['/images/project/edutainment-1.png','/images/project/edutainment-2.png',
-                  '/images/project/edutainment-3.png','/images/project/edutainment-4.png'
+    'imagePath':['assets/images/project/edutainment-1.png','assets/images/project/edutainment-2.png',
+                  'assets/images/project/edutainment-3.png','assets/images/project/edutainment-4.png'
                   ]
 
 },
 {
     'title':'Record Communication UIUX Design',
     'description':'In the Niya Sports Wear project, I faced a complex issue with state ',
-    'imagePath':['/images/project/recordui-1.png','/images/project/recordui-2.png',
+    'imagePath':['assets/images/project/recordui-1.png','assets/images/project/recordui-2.png',
                   
                   ]
 
